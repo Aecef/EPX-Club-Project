f30ff3fb0dee0ac6bd86426de4316e306cd2c67f
Alec <acleofe@uiowa.edu>
2021-04-02 18:37:00 -0500

    Added Ability to activate shield with Q

---
+++ ClubProject2/Assets/Player_Ablities.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Ablities : MonoBehaviour
+{
+
+    private Rigidbody2D rb;
+    private Animator anim;
+    private SpriteRenderer sr;
+    public GameObject shield;
+    public bool invincible = false;
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+        //Obtains info from the player sprite
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        sr = GetComponent<SpriteRenderer>();
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.Q))
+        {
+            Instantiate(shield);
+        }
+
+    }
+}
---
+++ ClubProject2/Assets/Shield.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class Shield : MonoBehaviour
+{
+    private float liveTime = 2.0f;
+    public Transform player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        //Attaches the shield to the "Player" gameobject aka locks it to the postion of the player
+        transform.position = player.transform.position;
+        transform.SetParent(player);
+
+    }
+    void Update()
+    {
+        liveTime -= Time.deltaTime;
+        if (liveTime <= 0)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
